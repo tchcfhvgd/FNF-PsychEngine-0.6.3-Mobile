6fb86fb6554355ce49c6f3267bdc0a2d149c02a1
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-06-30 13:36:36 +0800

    Add files via upload

--- source/InputFormatter.hx
+++ source/InputFormatter.hx
@@ -1,11 +1,16 @@
+package input;
+
 import flixel.FlxG;
 import flixel.input.keyboard.FlxKey;
 
 using StringTools;
 
-class InputFormatter {
-	public static function getKeyName(key:FlxKey):String {
-		switch (key) {
+class InputFormatter
+{
+	public static function getKeyName(key:FlxKey):String
+	{
+		switch (key)
+		{
 			case BACKSPACE:
 				return "BckSpc";
 			case CONTROL:
@@ -72,13 +77,13 @@ class InputFormatter {
 				return ",";
 			case PERIOD:
 				return ".";
-			//case SLASH:
+			// case SLASH:
 			//	return "/";
 			case GRAVEACCENT:
 				return "`";
 			case LBRACKET:
 				return "[";
-			//case BACKSLASH:
+			// case BACKSLASH:
 			//	return "\\";
 			case RBRACKET:
 				return "]";
@@ -90,7 +95,8 @@ class InputFormatter {
 				return '---';
 			default:
 				var label:String = '' + key;
-				if(label.toLowerCase() == 'null') return '---';
+				if (label.toLowerCase() == 'null')
+					return '---';
 				return '' + label.charAt(0).toUpperCase() + label.substr(1).toLowerCase();
 		}
 	}
