1a139ce7e6e07e33df2810cd6c0f7ee1fc239aec
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-07-30 10:37:59 +0800

    Add files via upload

---
+++ example_mods/data/recurrence/script.lua
@@ -0,0 +1,17 @@
+
+function onUpdate()
+
+
+
+        noteTweenX('wut', 4, 90, 0.0005)
+        noteTweenX('wut2', 5, 205, 0.0005)
+        noteTweenX('wut3', 6, 315, 0.0005)
+        noteTweenX('wut4', 7, 428, 0.0005)
+
+        noteTweenX('wutOp', 0, 730, 0.0005)
+        noteTweenX('wutOp2', 1, 845, 0.0005)
+        noteTweenX('wutOp3', 2, 954, 0.0005)
+        noteTweenX('wutOp4', 3, 1070, 0.0005)
+
+
+end
