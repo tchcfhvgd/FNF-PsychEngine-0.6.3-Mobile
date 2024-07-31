ae6d8c48d0f57234e4bf18fb2b5d3b98a988d843
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-07-31 13:44:09 +0800

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -340,11 +340,11 @@ class TitleState extends MusicBeatState
 		var path = "mods/" + Paths.currentModDirectory + "/images/titleEnter.png";
 		//trace(path, FileSystem.exists(path));
 		if (!FileSystem.exists(path)){
-			path = "mods/images/titleEnter.png";
+			path = Sys.getCwd() + "mods/images/titleEnter.png";
 		}
 		//trace(path, FileSystem.exists(path));
 		if (!FileSystem.exists(path)){
-			path = "assets/images/titleEnter.png";
+			path = Sys.getCwd() + "assets/images/titleEnter.png";
 		}
 		//trace(path, FileSystem.exists(path));
 		titleText.frames = FlxAtlasFrames.fromSparrow(BitmapData.fromFile(path),File.getContent(StringTools.replace(path,".png",".xml")));
